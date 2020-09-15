1235a1682fbf88ce1e72dc89665ae6683fd4487d
Tim Mahoney <timmahoney2000@gmail.com>
2020-09-15 16:32:13 -0400

    1

--- server.js
+++ server.js
@@ -1,13 +1,28 @@
+require('dotenv').config()
+const movies = require('./movies-data-small')
 const express = require('express')
 const morgan = require('morgan')
+console.log(process.env.API_TOKEN)
 
 const app = express()
 
 app.use(morgan('dev'))
 
-app.use((req, res) => {
-  res.send('Hello, world!')
-})
+function handleGetMovies(req, res) {
+    const validTypes = ['filmtv_ID', 'film_title', 'year', 'genre', 'duration', 'country', 'director', 'actors', 'avg_vote', 'votes']
+
+    function handleGetMovies(req, res) {
+        res.json
+    }
+}
+
+app.get('/movies', handleGetMovies)
+
+function handleGetMovieGetMovies(req, res) {
+    res.send('Hello, Movies!')
+}
+
+app.get('movies', handleGetMovies)
 
 const PORT = 8000
 
